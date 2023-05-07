2c9a79deef5447808de48ed3ad7ae2c890bcdf6c
Kanawut Netpol <kanawut.netpol@kingpower.com>
2023-05-07 19:04:04 +0700

    update banner 7/5/65

--- src/pages/Home/components/promotions.js
+++ src/pages/Home/components/promotions.js
@@ -20,12 +20,12 @@ export default class Promotions extends Component {
             </div>
             <div class="col-lg-6 col-sm-12 col-12">
               <a href="https://lin.ee/3qY2SjL" target="blank">
-                <img alt="aisfibre promotion" src="/images/promotionBanner/promotion-banner-3.jpg"/>
+                <img alt="aisfibre promotion" src="/images/promotionBanner/promotion-banner-2.jpg"/>
               </a>
             </div>
             <div class="col-lg-6 col-sm-12 col-12">
               <a href="https://lin.ee/3qY2SjL" target="blank">
-                <img alt="aisfibre promotion" src="/images/promotionBanner/promotion-banner-2.jpg"/>
+                <img alt="aisfibre promotion" src="/images/promotionBanner/promotion-banner-3.jpg"/>
               </a>
             </div>
             <div class="col-lg-6 col-sm-12 col-12">
